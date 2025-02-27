248fc2ea0cb6afaff569e14333b4bc32296423e8
Stella Gonçalvese <stellagmendonca@gmaile.com>
2025-02-27 17:04:48 -0300

    adicionei minha ft

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+const conteiner = document.querySelector('.conteiner-imagens'); // Um único contêiner
+const btnProximo = document.querySelector('.btn-proximo');
+const btnAnterior = document.querySelector('.btn-anterior');
+
+let index = 0;
+const totalImagens = document.querySelectorAll(".slider").length;
+const imagensVisiveis = 4;
+const larguraImagem = document.querySelector(".slider").clientWidth;
+
+btnProximo.addEventListener("click", () => {
+    if (index < totalImagens - imagensVisiveis) {
+        index++;
+        conteiner.style.transform = `translateX(-${index * (larguraImagem + 310)}px)`;
+    }
+});
+
+btnAnterior.addEventListener("click", () => {
+    if (index > 0) {
+        index--;
+        conteiner.style.transform = `translateX(-${index * (larguraImagem + 10)}px)`;
+    }
+});
+
+
 function escrever() {
     const texto = document.getElementById("texto-sobre-mim").innerHTML;
     const texto2 = document.getElementById("texto-contatos").innerHTML;
+
     let i1 = 0; // Para o primeiro texto
     let i2 = 0; // Para o segundo texto
 
@@ -20,7 +45,7 @@ function escrever() {
         } else {
             document.getElementById("texto").style.borderRight = "none"; // Remove o cursor quando termina
             document.getElementById("texto2").style.borderRight = "none"; // Remove o cursor quando termina
-        }
+     }
     }
 
     digitar();
@@ -29,34 +54,15 @@ function escrever() {
 // Função que verifica a rolagem da página e dispara a digitação
 window.onscroll = function() {
     const textoDiv = document.getElementById("texto");
-    const textoDiv2 = document.getElementById("texto2");
     const textoDivPos = textoDiv.getBoundingClientRect().top;
+    const textoDiv2 = document.getElementById("texto2");
     const textoDiv2Pos = textoDiv2.getBoundingClientRect().top;
     const windowHeight = window.innerHeight;
 
     // Inicia o efeito quando qualquer uma das divs estiver visível na tela
-    if ((textoDivPos < windowHeight) || (textoDiv2Pos < windowHeight)) {
+    if ((textoDivPos < windowHeight) || (textoDiv2Pos < windowHeight) ) {
         escrever();
         window.onscroll = null; // Desativa o evento após iniciar o efeito
     }
 };
 
-const slider = document.querySelectorAll('.slider');
-const btnProximo = document.querySelector('.btn-proximo');
-const btnAnterior = document.querySelector('.btn-anterior');
-
-let slideAtivo = 0;
-
-function EscondeSlide()
-{
-    slider.forEach(item => item.classList.remove('on'))
-}
-
-function MostraSlide()
-{
-    slider[slideAtivo].classList.add('on');
-
-}
-
-btnProximo.addEventListener('click', () => console.log("clicado"));
-// btnAnterior.addEventListener('click');
